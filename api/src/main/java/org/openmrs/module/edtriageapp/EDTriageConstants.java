a872246afc5ed35a597205c5e2821ccc9f66a0e0
HerbertYiga <herbertyiga@gmail.com>
2022-08-08 14:42:48 +0300

    upate

--- api/src/main/java/org/openmrs/module/edtriageapp/EDTriageConstants.java
+++ api/src/main/java/org/openmrs/module/edtriageapp/EDTriageConstants.java
@@ -9,7 +9,7 @@ public class EDTriageConstants {
     public static final String TRIAGE_QUEUE_WAITING_FOR_EVALUATION_CONCEPT_UUID = "90b571f3-5a4e-4634-903b-e322888c469a";
     public static final String TRIAGE_QUEUE_EXPIRED_CONCEPT_UUID = "99928547-6b1e-4ccf-9a43-2c71c527f8eb";
     public static final String TRIAGE_WAITING_TIME_UUID = "0a84ea93-e057-4643-abe6-dca97426da8d";
-    public static final String ED_TRIAGE_ENCOUNTER_TYPE_UUID = "1ca35231-14d6-45c1-b380-14a4d02115eb";
+    public static final String ED_TRIAGE_ENCOUNTER_TYPE_UUID = "bf715c87-eda5-49df-b1b1-08639da1fdcc";
     public static final String EDTRIAGE_VERSION_GP = "edtriage_metadata.installedVersion";
     public static final Integer EDTRIAGE_METADATA_VERSION = 1;
     public static final String TRIAGE_CONCEPT_SET_OF_SETS = "HUM Triage set";
